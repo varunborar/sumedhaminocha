d866e81e857a8aab32857a6e12adbf703b1e85eb
varunborar <varunborar@gmail.com>
2025-10-25 23:36:21 +0530

    initial commit

---
+++ src/app/page.js
@@ -0,0 +1,10 @@
+
+import { NavBar } from "@/components/navbar/navbar";
+
+export default function Home() {
+  return (
+    <div>
+      <NavBar/>
+    </div>
+  );
+}
---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,38 @@
+"use client"
+
+import * as React from "react"
+import Link from "next/link"
+import { CircleCheckIcon, CircleHelpIcon, CircleIcon } from "lucide-react"
+
+import { useIsMobile } from "@/hooks/use-mobile"
+import {
+    NavigationMenu,
+    NavigationMenuContent,
+    NavigationMenuItem,
+    NavigationMenuLink,
+    NavigationMenuList,
+    NavigationMenuTrigger,
+    navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu"
+
+export function NavBar() {
+    const isMobile = useIsMobile()
+
+    return (
+        <NavigationMenu viewport={isMobile}>
+            <NavigationMenuList className="flex-wrap">
+                <NavigationMenuItem>
+                    <NavigationMenuLink asChild className={navigationMenuTriggerStyle()}>
+                        <Link href="/">Home</Link>
+                    </NavigationMenuLink>
+                </NavigationMenuItem>
+                <NavigationMenuItem>
+                    <NavigationMenuLink asChild className={navigationMenuTriggerStyle()}>
+                        <Link href="/">Research</Link>
+                    </NavigationMenuLink>
+                </NavigationMenuItem>
+            </NavigationMenuList>
+        </NavigationMenu>
+    )
+}
+
